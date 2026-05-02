1acf4028921f99b99de207ce56e9c495dc5457f2
Abhinandan Maji <abhinandanmaji01@gmail.com>
2026-05-02 23:24:20 +0530

    Adding the components

---
+++ frontend/src/landing_page/products/ProductPage.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Universe from "./Universe";
+
+function ProductPage() {
+  return (
+    <>
+      <Hero />
+      <LeftSection
+        imageURL="media/images/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection
+        imageURL="media/images/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+      />
+      <LeftSection
+        imageURL="media/images/coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account.Enjoy the investment experience on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection
+        imageURL="media/images/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+      />
+      <LeftSection
+        imageURL="media/images/varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustration. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+
+      <p>
+        Want to know more about our technology stack? Check out the Zerodha.Tech
+        blog.
+      </p>
+      <Universe />
+    </>
+  );
+}
+
+export default ProductPage;
---
+++ frontend/src/landing_page/products/Universe.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function Universe() {
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <h1> The Zerodha Universe</h1>
+        <p className="mt-5 mb-5 text-center">
+          Extend your trading and investment experience even futher with our
+          parthner platforms.
+        </p>
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/smallcaseLogo.png" />
+          <p className="text-small text-muted">Thematic investment platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/smallcaseLogo.png" />
+          <p className="text-small text-muted">Thematic investment platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/smallcaseLogo.png" />
+          <p className="text-small text-muted">Thematic investment platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/smallcaseLogo.png" />
+          <p className="text-small text-muted">Thematic investment platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/smallcaseLogo.png" />
+          <p className="text-small text-muted">Thematic investment platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/smallcaseLogo.png" />
+          <p className="text-small text-muted">Thematic investment platform</p>
+        </div>
+        <button
+          className="p-2 btn btn-primary"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Universe;
